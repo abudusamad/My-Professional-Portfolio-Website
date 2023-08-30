9cd988cb395da96ba3d9e2d2c5c51ee1e97716a7
Mastech <82134715+abudusamad@users.noreply.github.com>
2023-08-30 18:13:20 +0000

    working with the response from the visitor when filled the form

--- js/buttonSubmission.js
+++ js/buttonSubmission.js
@@ -1,16 +1,19 @@
-const form = document.getElementById("myForm");
 document.addEventListener("DOMContentLoaded", function () {
+	const form = document.getElementById("myForm");
 	const responseDiv = document.getElementById("response");
+
 	form.addEventListener("submit", function (event) {
-		event.preventDefault();
+		event.preventDefault(); // Prevent the default form submission behavior
 
 		const name = document.getElementById("name").value;
 		const email = document.getElementById("email").value;
 
+		// Process the form data (you can replace this with your own logic)
 		const responseData = {
-			message: "Thank you, ${name}! Your email (${email}) has been received.",
+			message: `Thank you, ${name}! Your email (${email}) has been received.`,
 		};
 
+		// Display the response
 		responseDiv.textContent = responseData.message;
 	});
 });
