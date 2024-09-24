1439e7c0ce97f2e51c8c9f8aeb64efeb7a88f52e
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-09-24 15:11:22 +0000

    Addng the auth0 for github 🔒

--- src/auth.ts
+++ src/auth.ts
@@ -1,15 +1,12 @@
 import NextAuth from "next-auth"
-import Github from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+import authConfig from "./auth.config"
  
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Github({
-    clientId: process.env.AUTH_GITHUB_ID!,
-    clientSecret: process.env.AUTH_GITHUB_SECRET!,
-  }),
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    })
-  ],
+const prisma = new PrismaClient()
+ 
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: { strategy: "jwt" },
+  ...authConfig,
 })
