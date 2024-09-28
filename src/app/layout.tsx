384d6de7110659218718d841b5567f6ea80693c6
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-09-28 23:02:13 +0000

    making the the herosection 🙍‍♂️

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+          {children}
+        </main>
       </body>
     </html>
   );
