424d923a8a79a6247bb6d1285db425519e30bdba
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-09 23:33:11 +0000

    Making the api route for unpublish course

--- src/app/api/admin/projects/[projectId]/unpublish/route.ts
+++ src/app/api/admin/projects/[projectId]/unpublish/route.ts
@@ -1,11 +1,14 @@
+import { NextResponse } from "next/server";
+
+
 import getCurrentUser from "@/actions/get-current-user";
 import { db } from "@/lib/db";
-import { NextResponse } from "next/server";
+
 
 export async function PATCH(req: Request, { params }: { params: { projectId: string } }) {
     try {
         const currentUser = await getCurrentUser();
-        if(!currentUser) {
+        if (!currentUser) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
         const project = await db.project.findUnique({
@@ -15,26 +18,24 @@ export async function PATCH(req: Request, { params }: { params: { projectId: str
             }
         });
 
-        if(!project) {
+        if (!project) {
             return new NextResponse("Not Found", { status: 404 });
-        }
-
-        if(!project.title || !project.description || !project.image_url || !project.techId || !project.link) {
-            return new NextResponse("Bad Request", { status: 400 });
         }
 
         const updatedProject = await db.project.update({
             where: {
-                id: params.projectId
+                id: params.projectId,
+                userId: currentUser.id,
             },
             data: {
-                isPublished: true
+                isPublished:false
             }
         });
 
         return NextResponse.json(updatedProject);
         
     } catch (error) {
+        console.log(["PORJECT_UNPUBLISH_ERROR", error]);
         return new NextResponse("Internal Server Error", { status: 500 });
     }
 }
