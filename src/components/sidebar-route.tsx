befa2b274909df00f0c79ccd97f60b9198f301ce
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-09 20:52:17 +0000

    adding the modules

--- src/components/sidebar-route.tsx
+++ src/components/sidebar-route.tsx
@@ -100,31 +100,33 @@ export const SidebarRoutes = () => {
           href={route.href}
         />
       ))}
-      <div className="flex items-center justify-center ">
-        <Card className="mt-28 bg-transparent w-72 md:w-64 ">
-          <CardHeader>
-            <CardTitle>
-              <p className="text-amber-300 ">
-                {session ? "Welcome Back!" : "Join Us"}
-              </p>
-            </CardTitle>
-            <CardDescription>
-              {session
-                ? "You are currently signed in. Click below to sign out."
-                : "Sign in to access more features and personalize your experience."}
-            </CardDescription>
-          </CardHeader>
-          <CardContent>
-            <Button
-              variant="default"
-              className="w-full  text-sm font-medium bg-pink-500 hover:bg-pink-600 text-white"
-              onClick={() => (session ? signOut() : signIn())}
-            >
-              {session ? "Sign Out" : "Sign In"}
-            </Button>
-          </CardContent>
-        </Card>
-      </div>
+      {!isAdmin && (
+        <div className="flex items-center justify-center ">
+          <Card className="mt-28 bg-transparent w-72 md:w-64 ">
+            <CardHeader>
+              <CardTitle>
+                <p className="text-amber-300 ">
+                  {session ? "Welcome Back!" : "Join Us"}
+                </p>
+              </CardTitle>
+              <CardDescription>
+                {session
+                  ? "You are currently signed in. Click below to sign out."
+                  : "Sign in to access more features and personalize your experience."}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Button
+                variant="default"
+                className="w-full  text-sm font-medium bg-pink-500 hover:bg-pink-600 text-white"
+                onClick={() => (session ? signOut() : signIn())}
+              >
+                {session ? "Sign Out" : "Sign In"}
+              </Button>
+            </CardContent>
+          </Card>
+        </div>
+      )}
     </div>
   );
 };
