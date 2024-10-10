368af588de99dbb7663d6b8fa2e200362b677f9b
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-10 14:43:27 +0000

    Making the data-table for the project element

--- src/components/navbar-route.tsx
+++ src/components/navbar-route.tsx
@@ -7,32 +7,35 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 import { UserMenu } from "./user-menu";
+import { useCurrentUserRole } from "@/hooks/use-current-role";
+import { UserRole } from "@prisma/client";
 
 export const NavbarRoutes = () => {
   const pathname = usePathname();
 
   const isAdmin = pathname?.startsWith("/admin");
   const isCoursePage = pathname?.includes("/courses");
+  const currentUserRole = useCurrentUserRole();
 
-  return (
-   
-      <div className="flex gap-x-2  items-center justify-end">
-        {isAdmin || isCoursePage ? (
-          <Link href="/">
-            <Button size="sm" variant="ghost">
-              <LogOut className="h-4 w-4 mr-2" />
-              Exit
-            </Button>
-          </Link>
-        ) : (
-          <Link href="/admin">
-            <Button size="sm" variant="blue">
-              Admin
-            </Button>
-          </Link>
-        )}
-        <UserMenu />
-      </div>
-    
-  );
+ return (
+   <div className="flex items-center justify-end gap-x-2">
+     {(isAdmin || isCoursePage) && currentUserRole === UserRole.ADMIN ? (
+       <Link href="/">
+         <Button size="sm" variant="ghost">
+           <LogOut className="h-4 w-4 mr-2" />
+           Exit
+         </Button>
+       </Link>
+     ) : (
+       currentUserRole === UserRole.ADMIN && (
+         <Link href="/admin">
+           <Button size="sm" variant="blue">
+             Admin
+           </Button>
+         </Link>
+       )
+     )}
+     <UserMenu />
+   </div>
+ );
 };
