41319cd5a0ff9a39b519a0ea86329a41502e70b8
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-10 02:08:35 +0000

    making the page dynamic

--- src/actions/get-project.ts
+++ src/actions/get-project.ts
@@ -1,6 +1,7 @@
 
 
 import { db } from "@/lib/db";
+import redis from "@/lib/redis";
 
 
 type GetPrject = {
@@ -12,8 +13,17 @@ export const getProject = async ({
     title,
     techId,
 }: GetPrject)=> {
-	try {
-		const project = await db.project.findMany({
+    try {
+    //     const cacheKey = `projects:${title || ''}:${techId || ''}`;
+        
+    //         const cachedData = await redis.get(cacheKey);
+    //     if (cachedData) {
+    //         console.log("Cache hit:", cacheKey);
+    //         console.log("Cached data:", JSON.parse(cachedData));
+    //   return JSON.parse(cachedData);
+    // }
+
+		const projects = await db.project.findMany({
 			where: {
                 isPublished: true,
 				title: {
@@ -26,9 +36,12 @@ export const getProject = async ({
 			orderBy: {
 				createdAt: "desc",
 			},
-		});
-
-        return project
+        });
+        
+        // await redis.set(cacheKey, JSON.stringify(projects), 'EX', 3600);
+        // console.log("Cache miss:", cacheKey);
+        // console.log("Data cached:", projects);
+        return projects
 			
 	} catch (error) {
 		console.log("[GET_COURSES]", error);
