bfdac37381591b4f6b75647c46b5ce1ebe77c4ae
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-17 02:46:05 +0000

    Adding the authentication

--- src/components/auth/header.tsx
+++ src/components/auth/header.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { Poppins } from "next/font/google";
+import Image from "next/image";
 
 const font = Poppins({
   subsets: ["latin"],
@@ -13,7 +14,10 @@ interface HeaderProps {
 export const Header = ({ label }: HeaderProps) => {
   return (
     <div className="w-full flex flex-col gap-y-4 items-center justify-center">
-      <h1 className={cn("text-3xl font-semibold", font.className)}>🔐 Auth</h1>
+      <h1 className={cn("text-3xl font-semibold flex  items-center  space-x-4", font.className)}>
+        <Image src="/logo.png" alt="logo" height={45} width={45}/>
+        Abudu
+      </h1>
       <p className="text-muted-foreground text-lg">{label}</p>
     </div>
   );
