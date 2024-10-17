ba2368c24d82703f5fe7c3f741e72bb9202a64a0
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-17 14:32:01 +0000

    Making notification for the navbar 🔒

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,1 +1,24 @@
-export { auth as middleware } from "@/auth"
+// src/middleware.ts
+import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt';
+import { NextRequest } from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+
+  // Check if the user is authenticated
+  if (!token) {
+    return NextResponse.redirect(new URL('/auth/login', req.url));
+  }
+
+  // Check if the user has the "ADMIN" role
+  if (token.role !== 'ADMIN') {
+    return NextResponse.redirect(new URL('/unauthorized', req.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/admin/:path*'],
+};
