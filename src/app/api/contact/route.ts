e0220679116d603ab782868bbde260dc1f525412
Mastech <82134715+abudusamad@users.noreply.github.com>
2024-10-18 23:08:55 +0000

    Adding the api route to handle client request 💻

---
+++ src/app/api/contact/route.ts
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { NextResponse } from "next/server";
+
+export async function POST(request:Request) {
+  const payload = await request.json();
+  const token = process.env.TELEGRAM_BOT_TOKEN;
+  const chat_id = process.env.TELEGRAM_CHAT_ID;
+
+  if (!token || !chat_id) {
+    return NextResponse.json({
+      success: false,
+    }, { status: 200 });
+  };
+
+  try {
+    const url = `https://api.telegram.org/bot${token}/sendMessage`;
+    const message = `New message from ${payload.name}\n\nEmail: ${payload.email}\n\nMessage:\n ${payload.message}\n\n`;
+
+    const res = await axios.post(url, {
+      text: message,
+      chat_id: process.env.TELEGRAM_CHAT_ID
+    });
+
+    if (res.data.ok) {
+      return NextResponse.json({
+        success: true,
+        message: "Message sent successfully!",
+      }, { status: 200 });
+    };
+  } catch (error) {
+      console.log("MESSAGE ERROR", error)
+      return new NextResponse("Internal Server Error", {status: 500});
+  }
+};
